555f1f2ee7fe906edeb8ab9626424359bd078f27
konbraphat51 <konbraphat@gmail.com>
2024-06-12 17:33:14 +0900

    LoadImage()

--- Src/main.js
+++ Src/main.js
@@ -1,1 +1,10 @@
+async function LoadImage(event) {
+	const file = event.target.files[0]
+	const imageUrl = URL.createObjectURL(file)
+
+	const img = (await LoadImages([imageUrl]))[0]
+
+	DrawImage(img, 0, 0, 800, 600)
+}
+
 async function main() {}
