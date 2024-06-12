260078946f87ab56e9512c6e00e0d12c55b09e1b
konbraphat51 <konbraphat@gmail.com>
2024-06-12 17:35:30 +0900

    refac

--- Src/main.js
+++ Src/main.js
@@ -1,10 +1,14 @@
+var img
+
 async function LoadImage(event) {
 	const file = event.target.files[0]
 	const imageUrl = URL.createObjectURL(file)
 
-	const img = (await LoadImages([imageUrl]))[0]
+	img = (await LoadImages([imageUrl]))[0]
 
-	DrawImage(img, 0, 0, 800, 600)
+	DrawOriginal()
 }
 
-async function main() {}
+function DrawOriginal() {
+	DrawImage(img, 0, 0, 800, 600)
+}
