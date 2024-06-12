898ea35b9c5a5971978b48c2377d37774a99ffae
konbraphat51 <konbraphat@gmail.com>
2024-06-12 19:57:35 +0900

    adjust value

--- Src/main.js
+++ Src/main.js
@@ -13,7 +13,13 @@ function DrawOriginal() {
 	DrawImage(img, 0, 0, 800, 600)
 }
 
-function RidBrightness(v) {
+function GetValue() {
+	return document.getElementById("ValueInput").value
+}
+
+function RidBrightness() {
+	let v = GetValue()
+
 	let colors = PickAllColor()
 
 	//per pixel
@@ -26,6 +32,35 @@ function RidBrightness(v) {
 
 		//adjust brightness
 		hsv[2] = v
+
+		//re-convert to rgb
+		let rgb_altered = Hsv2Rgb(hsv[0], hsv[1], hsv[2])
+
+		//set color
+		colors[cnt][0] = rgb_altered[0]
+		colors[cnt][1] = rgb_altered[1]
+		colors[cnt][2] = rgb_altered[2]
+	}
+
+	//deploy
+	SetAllColor(colors)
+}
+
+function RidSaturation() {
+	let s = GetValue()
+
+	let colors = PickAllColor()
+
+	//per pixel
+	for (let cnt = 0; cnt < colors.length; cnt++) {
+		//get color
+		let rgb = [colors[cnt][0], colors[cnt][1], colors[cnt][2]]
+
+		//convert to hsv
+		let hsv = Rgb2Hsv(rgb[0], rgb[1], rgb[2])
+
+		//adjust saturation
+		hsv[1] = s
 
 		//re-convert to rgb
 		let rgb_altered = Hsv2Rgb(hsv[0], hsv[1], hsv[2])
